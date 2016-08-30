3cf7a2ab4281e2879173207d447bb635ab92b7dd
Russell Smith <rsmith@cern.ch>
2016-08-30 11:13:18 -0400

    running gendermodel

--- gendermodel.py
+++ gendermodel.py
@@ -23,6 +23,51 @@ def gendermodel():
 
     print(data)
     print(data[0])
+    number_passengers = np.size(data[0::,1].astype(np.float))
+    number_survived = np.sum(data[0::,1].astype(np.float))
+    proportion_survivors = number_survived / number_passengers
 
+    print(number_passengers)
+    print(number_survived)
+    print(proportion_survivors)
+
+    women_only_stats = data[0::,4] == "female" # This finds where all
+    # the elements in the gender
+    # column that equals “female”
+    men_only_stats = data[0::,4] != "female"   # This finds where all the
+    # elements do not equal
+    # female (i.e. male)
+
+    # Using the index from above we select the females and males separately
+    women_onboard = data[women_only_stats,1].astype(np.float)
+    men_onboard = data[men_only_stats,1].astype(np.float)
+
+    # Then we finds the proportions of them that survived
+    proportion_women_survived = np.sum(women_onboard) / np.size(women_onboard)
+    proportion_men_survived = np.sum(men_onboard) / np.size(men_onboard)
+
+    # and then print it out
+    print('Proportion of women who survived is %s' % proportion_women_survived)
+    print('Proportion of men who survived is %s' % proportion_men_survived)
+    
+    test_file = open('csv/test.csv', 'r')
+    test_file_object = csv.reader(test_file)
+    header = test_file_object.__next__()
+
+    prediction_file = open("genderbasedmodel.csv", "w")
+    prediction_file_object = csv.writer(prediction_file)
+
+    prediction_file_object.writerow(["PassengerId", "Survived"])
+    for row in test_file_object:       # For each row in test.csv
+        if row[3] == 'female':         # is it a female, if yes then
+            prediction_file_object.writerow([row[0],'1'])    # predict 1
+        else:                              # or else if male,
+            prediction_file_object.writerow([row[0],'0'])    # predict 0
+
+    print('closing files')
+    test_file.close()
+    prediction_file.close()
+    
+    
 if __name__ == "__main__":
     gendermodel()
