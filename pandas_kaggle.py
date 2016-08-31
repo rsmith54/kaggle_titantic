8e8d5794183f66fdeadc08e23a8517c87b27c050
Russell Smith <rsmith@cern.ch>
2016-08-31 14:20:24 -0400

    finishing pandas kaggle tut

--- pandas_kaggle.py
+++ pandas_kaggle.py
@@ -30,17 +30,34 @@ def pandas_smart_kaggle():
     print(df.loc[15,'Age'])
     print(df.info())
 
-    print(df[['Age', 'Cabin', 'Fare']])
-    print('mean : ', df['Age'].mean())
-    print('median : ', df['Age'].median())
-
-    print(df[ (df['Age'] > 60) & (df['Survived'] == 1)]  )
-
     df['Gender']   = df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
-    print(df['Embarked'].head(10))
     df['Embarked'] = df['Embarked'].map( lambda x: (1 if x == 'S' else 0) ).astype(int)
-#    print(df['Gender'])
-    print(df['Embarked'].head(10))
+    median_ages = np.zeros((2,3))
+    for i in range(0, 2):
+        for j in range(0, 3):
+            median_ages[i,j] = df[(df['Gender'] == i) & \
+                                  (df['Pclass'] == j+1)]['Age'].dropna().median()
+    
+    df['AgeFill'] = df['Age']
+    for i in range(0, 2):
+        for j in range(0, 3):
+            df.loc[ (df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j+1),\
+                    'AgeFill'] = median_ages[i,j]
+    df['AgeIsNull'] = pd.isnull(df.Age).astype(int)
+    
+    df['FamilySize'] = df['SibSp'] + df['Parch']
+    df['Age*Class'] = df.AgeFill * df.Pclass
+
+    df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked' , 'Age'], axis=1)
+
+    train_data = df.values
+    
+    # plt.figure()
+    # df['Age'].hist()
+    # plt.figure()
+    # df['AgeFill'].hist()
+    # plt.show()    
+    
     
 if __name__ == '__main__':
 #    pandas_kaggle()
